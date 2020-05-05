9b9f769642d6f89b03ad969d5fd42e673673de09
muwahidasdim <64785354+muwahidasdim@users.noreply.github.com>
2020-05-05 17:39:32 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,55 @@
 #include <iostream>
+#include<string>
 using namespace std;
+
+class StringManip
+{
+private:
+    char* array[50];
+    int size = 0;
+    string s = " ";
+public:
+    StringManip()
+    {
+        size = 50;
+
+    }
+    StringManip(string s1)
+    {
+        s = s1;
+
+    }
+    StringManip(int a)
+    {
+        size = a;
+        char* arr = new char[size];
+
+    }
+    ~StringManip()
+    {
+        cout << "Destructor Called!";
+    }
+
+};
 
 int main()
 {
-    cout << "Hello, this is Saaim";
-    cout << "Hello, this is Muwahid";
-    cout << "Do you see this Saaim";
-    cout << "Hello, this is Mujahid";
-    cout << "Saaim agr yeh parha to tum gaandu";
+    int a = 0;
+    string  s1;
+    StringManip obj1;
+
+
+    cout << "\nEnter string:    ";
+    getline(cin, s1);
+    StringManip obj2(string s1);
+
+
+    cout << "\nEnter array size:     ";
+    cin.ignore();
+    StringManip obj3(int a);
+
+
+
+
     return 0;
 }
