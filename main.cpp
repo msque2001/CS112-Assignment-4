bc946358d1a0b74f82a1001ee16740ab59748cbd
Muhammad Saaim Qureshi <saaimque@gmail.com>
2020-05-05 22:07:50 +0500

    Indetation fixed

--- main.cpp
+++ main.cpp
@@ -9,27 +9,27 @@ class StringManip
   public:
     StringManip()
       {
-          theString = new char[50];
+        theString = new char[50];
       }
 
-      StringManip(char s1[])
-      {
-        while(s1[array_size] != '\0')
-            array_size++;
+    StringManip(char s1[])
+    {
+      while(s1[array_size] != '\0')
+        array_size++;
 
-        theString = new char[array_size];
-        theString = s1;
-      }
+      theString = new char[array_size];
+      theString = s1;
+    }
 
-      StringManip(int string_size)
-      {
-          array_size = string_size;
-          theString = new char[array_size];
-      }
+    StringManip(int string_size)
+    {
+      array_size = string_size;
+      theString = new char[array_size];
+    }
 
     ~StringManip()
     {
-       delete this;
+      delete[] theString;
     }
 };
 
