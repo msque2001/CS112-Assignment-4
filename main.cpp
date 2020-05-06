975f88f95a182e2354674ace163919d11d3b33c2
Muhammad Saaim Qureshi <saaimque@gmail.com>
2020-05-06 20:59:36 +0500

    Added += operator

--- main.cpp
+++ main.cpp
@@ -5,6 +5,16 @@ class StringManip
 {
   char *theString;
   int array_size = 0;
+
+  int stringSize(char xString[])
+  {
+    int string_size = 0;
+    while(xString[string_size] != '\0')
+      string_size++;
+    
+    return string_size;
+  }
+
   public:
     StringManip()
       {
@@ -16,7 +26,7 @@ class StringManip
       while(s1[array_size] != '\0')
         array_size++;
 
-      theString = new char[array_size];
+      theString = new char[array_size]; // CHANGE
       
       for(int i = 0; i < array_size; i++)
         theString[i] = s1[i];
@@ -32,11 +42,49 @@ class StringManip
     {
       delete[] theString;
     }
+
+    int getStringSize()
+    {
+      int string_size = stringSize(theString);
+      return string_size;
+    }
+
+    void operator+=(const StringManip &other_string)
+    {
+      const int current_string_size = stringSize(theString);
+      const int other_string_size = stringSize(other_string.theString);
+      const int required_array_size = current_string_size + other_string_size;
+
+      if(required_array_size > array_size)
+      {
+        char temp[current_string_size];
+        
+        for(int i = 0; i < current_string_size; i++)
+          temp[i] = theString[i];
+
+        theString = new char[required_array_size];
+
+        for(int i = 0; i < current_string_size; i++)
+          theString[i] = temp[i];
+
+        for(int i = current_string_size, j = 0; i < required_array_size && j < other_string_size; i++, j++)
+          theString[i] = other_string.theString[j];
+      }
+
+      else // TEST THIS LATER!!!!!!!!!!
+      {
+          for(int i = current_string_size, j = 0; i < array_size && j < other_string_size; i++, j++)
+            theString[i] = other_string.theString[j];
+      }
+      
+      
+    }
 };
 
 int main()
 {
-  char myArr[] = "Saaim Qureshi";
-  StringManip obj(myArr);
+  char myArr[] = "Saaim", other[] = " Qureshi";
+  StringManip obj(myArr), x(other);
+
   return 0;
 }
